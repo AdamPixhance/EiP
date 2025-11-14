408a1876d45cc3726f1ee71cfb595bfb072b696f
Adam Alkhatib <126474809+AdamPixhance@users.noreply.github.com>
2025-11-14 14:30:49 +0100

    finished the 2) Punkte java file

--- Übungsblätter/ex6/Point.java
+++ Übungsblätter/ex6/Point.java
@@ -25,7 +25,18 @@ public class Point {
 	// Measure distance between 2 points
 	public double distanceTo(Point other) {
 		double distance = 0;
+		// Measuring the distance between the points on the y and x axis
+		double SeiteX = Math.abs(x - other.x);
+		double SeiteY = Math.abs(y - other.y);
 
+		// Measuring distance with the use of my previous Pythagoras file, from Aufgabe1
+		Pythagoras.calculate(SeiteX, SeiteY);
 		return distance;
+	}
+
+	// Move (Shift) a point on the x and y axis
+	public void shift(double xShift, double yShift) {
+		x += xShift;
+		y += yShift;
 	}
 }
