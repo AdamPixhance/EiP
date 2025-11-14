d7011ef2b7b102293cc574d25e1dde7e79c0fcd5
Adam Alkhatib <126474809+AdamPixhance@users.noreply.github.com>
2025-11-14 13:46:34 +0100

    pre lunch updates

---
+++ Übungsblätter/ex6/Point.java
@@ -0,0 +1,31 @@
+
+public class Point {
+	// x and y coordinates
+	private double x;
+	private double y;
+
+	// Getter and Setter for x
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	// Getter and Setter for y
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	// Measure distance between 2 points
+	public double distanceTo(Point other) {
+		double distance = 0;
+
+		return distance;
+	}
+}
