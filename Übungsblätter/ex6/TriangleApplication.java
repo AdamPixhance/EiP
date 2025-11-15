6ca3fd431335b83462a7da0bdead3033863de840
Adam Alkhatib <126474809+AdamPixhance@users.noreply.github.com>
2025-11-15 15:40:03 +0100

    15-11-2025- 15:30

--- Übungsblätter/ex6/TriangleApplication.java
+++ Übungsblätter/ex6/TriangleApplication.java
@@ -2,14 +2,15 @@
 public class TriangleApplication {
 
 	public static void main(String[] args) {
-		// this whole .java class, is used to debug the code in Point, Pythagoras and Triangle java files that are in the ex6 project
-		Point a = new Point(0,0);
-		Point b = new Point(0,4);
-		Point c = new Point(4,0);
-		Triangle test = new Triangle(a,b,c);
-		
-		System.out.println(test.isRight());
-		System.out.println(test.isObtuse());
+		// this whole .java class, is used to debug the code in Point, Pythagoras and
+		// Triangle java files that are in the ex6 project
+		Point a = new Point(0, 0);
+		Point b = new Point(0, 4);
+		Point c = new Point(4, 0);
+		Triangle test = new Triangle(a, b, c);
+
+		System.out.println("is Right gives back the value: " + test.isRight());
+		System.out.println("is obtuse gives back the value: " + test.isObtuse());
 
 	}
 
